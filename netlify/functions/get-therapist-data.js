958da26307c4616c145d5b97fae0e31c0e4d3285
Monica <monica@webflow.com>
2024-10-07 23:42:13 -0700

    Update get-therapist-data.js

--- netlify/functions/get-therapist-data.js
+++ netlify/functions/get-therapist-data.js
@@ -1,10 +1,11 @@
-const fetch = require('node-fetch');
-
 exports.handler = async function(event, context) {
   const WEBFLOW_API_KEY = 'fe828a50fc10216bfe01f9b84d4c20ce97c6f12522a52062d1a046606696b82e';
   const COLLECTION_ID = '6703fd91503b242fa5062b25';
 
   try {
+    // Dynamically import node-fetch
+    const fetch = (await import('node-fetch')).default;
+
     const response = await fetch(`https://api.webflow.com/collections/${COLLECTION_ID}/items?limit=100`, {
       method: 'GET',
       headers: {
