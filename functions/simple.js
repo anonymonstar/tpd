a20f449465154341b35e7c443718f26424304e9b
Monica <monica@webflow.com>
2024-10-07 20:33:31 -0700

    Update simple.js

--- functions/simple.js
+++ functions/simple.js
@@ -1,11 +1,6 @@
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',  // This allows any domain to access the resource
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
-        },
-        body: JSON.stringify({ message: "CORS headers are working!" })
+        body: JSON.stringify({ message: "Function is working!" })
     };
 };
